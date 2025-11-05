93f08ce93276a47e7ecfef1ac8b4739b89201e33
zelena001 <40139689+zelena001@users.noreply.github.com>
2025-11-05 22:27:01 +0700

    limit workflow and add gh pahe

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   timeout: 30_000,
+  workers: process.env.CI ? 2 : undefined,
   reporter: [["html", { open: "never", outputFolder: "playwright-report" }]],
   expect: {
     // ✅ Snapshot/visual testing tolerances
