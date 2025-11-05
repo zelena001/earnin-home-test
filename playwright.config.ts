0ad95f5ecbcca5e0e4ff1b89737ee6ff270497b3
zelena001 <40139689+zelena001@users.noreply.github.com>
2025-11-05 09:30:50 +0700

    fix no open

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   timeout: 30_000,
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  reporter: [['html', {  open: 'never',outputFolder: 'playwright-report' }]],
   expect: {
     // ✅ Snapshot/visual testing tolerances
     toHaveScreenshot: { maxDiffPixelRatio: 0.02 },
