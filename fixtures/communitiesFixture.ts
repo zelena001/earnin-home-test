78c1739071981b3c3e436e0a9ab20783cd823328
zelena001 <40139689+zelena001@users.noreply.github.com>
2025-11-06 07:41:36 +0700

    attempt 2, don't use goto in fixture

--- fixtures/communitiesFixture.ts
+++ fixtures/communitiesFixture.ts
@@ -1,22 +1,18 @@
 import { test as base, Page } from '@playwright/test';
 import { CommunityPage } from '../pages/CommunityPage';
 
-// Extend the base test to include a "community" fixture
 export const test = base.extend<{
   community: CommunityPage;
 }>({
   community: async ({ page }, use, testInfo) => {
     // Initialize CommunityPage once per test
     const communityPage = new CommunityPage(page, !!testInfo.project.use.isMobile);
-    
-    // Navigate to the page before each test
-    await communityPage.goto();
 
-     // Give the "community" instance to the test
-  await use(communityPage);
+    // Do NOT navigate here
+    await use(communityPage);
 
-  // Teardown (runs AFTER the test) antything from this line is teardown
-  console.log('Test finished');
+    // Teardown (runs AFTER the test)
+    console.log('Community test finished');
   }
 });
 
